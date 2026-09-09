99f5b0e5e1b4ee261f77818c4337a87284bd080e
hegedusmarcell15 <hegedusm@kkszki.hu>
2026-09-09 09:53:54 +0200

    Models created

--- Blog API/obj/Debug/net10.0/Blog API.AssemblyInfo.cs
+++ Blog API/obj/Debug/net10.0/Blog API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1ac815132af216627e605e0401d1cfe6a54a70e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
